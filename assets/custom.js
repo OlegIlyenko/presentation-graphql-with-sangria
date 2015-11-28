3027cb364938c1f4818f0addc9c33689e564bc3a
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2015-11-28 21:27:40 +0100

    Diagrams

--- assets/custom.js
+++ assets/custom.js
@@ -1,4 +1,8 @@
 $(function() {
+  //var graphiqlUrl = "http://localhost:9000/graphiql"
+  var graphiqlUrl = "http://try.sangria-graphql.org/graphiql"
+  var graphiqlZoom = 150
+
   var showHideCredits = function (currentSlide) {
     var section = $(currentSlide)
 
@@ -33,4 +37,22 @@ $(function() {
   Reveal.addEventListener( 'ready', function( event ) {
     showHideCredits(event.currentSlide)
   });
+
+  var initGraphiQL = function (selector) {
+    selector.map(function (idx, elem) {
+      var replaceElem = $(elem).parent().get(0)
+      var vars = $(elem).data("vars")
+      var varsParam = vars ? "variables=" +encodeURIComponent(vars) : "hideVariables=true"
+      var queryAndResp = $(elem).text().trim().split("// Response")
+      var query = queryAndResp[0].trim()
+      var resp = queryAndResp[1].trim()
+
+
+      $('<iframe src="' + graphiqlUrl + '?zoom=' + graphiqlZoom + '&query=' + encodeURIComponent(query) + '&response=' + encodeURIComponent(resp) + '&' + varsParam + '" class="graphiql">').insertAfter(replaceElem)
+
+      replaceElem.remove()
+    })
+  }
+
+  initGraphiQL($("pre code.graphiql"))
 })
